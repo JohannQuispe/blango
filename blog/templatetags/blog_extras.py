ae25fbdbc96e14756b99c4736cfe1ad858374795
Johann <jbquispe@hotmail.com>
2024-06-26 14:11:04 -0400

    escape author_detail filter

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 from django import template
 
 
@@ -7,14 +9,21 @@ register = template.Library()
 
 @register.filter
 def author_details(author):
-  
-   if not isinstance(author, user_model):
-       # return empty string as safe default
-       return ""
-  
-   if author.first_name and author.last_name:
-       name = f"{author.first_name} {author.last_name}"
-   else:
-       name = f"{author.username}"
-  
-   return name
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+    
+    if author.first_name and author.last_name:
+        name = escape(f"{author.first_name} {author.last_name}")
+    else:
+        name = escape(f"{author.username}")
+    
+    if author.email:
+        email = escape(author.email)
+        prefix = f'<a href="mailto:{email}">'
+        suffix = "</a>"
+    else:
+        prefix = ""
+        suffix = ""
+    
+    return mark_safe(f"{prefix}{name}{suffix}")
