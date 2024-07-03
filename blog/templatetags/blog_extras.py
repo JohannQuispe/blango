e50ba524f69168588cce450f404259f6d1311e09
Johann <jbquispe@hotmail.com>
2024-07-03 14:50:44 -0400

    simple tag with arguments for example on index = {% row 'border-bottom' %}

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -29,8 +29,8 @@ def author_details(author, current_user):
     
     
 @register.simple_tag
-def row():
-    return format_html('<div class="row">')
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
 @register.simple_tag
 def endrow():
     return format_html("</div>")
